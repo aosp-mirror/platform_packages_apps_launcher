8b498ce7c4d7bc25096fe004350e7dc587b8deae
chrisrl <chris+android@zenthought.org>
2009-10-26 13:30:03 -0500

    Fix a race condition that caused the Launcher to be blank when triggered.

--- src/com/android/launcher/LauncherModel.java
+++ src/com/android/launcher/LauncherModel.java
@@ -541,10 +541,12 @@ public class LauncherModel {
                 launcher.runOnUiThread(action);
             }
 
-            if (!mStopped) {
-                mApplicationsLoaded = true;
-            } else {
-                if (DEBUG_LOADERS) d(LOG_TAG, "  ----> applications loader stopped (" + mId + ")");                                
+            synchronized(LauncherModel.this) {
+                if (!mStopped) {
+                    mApplicationsLoaded = true;
+                } else {
+                    if (DEBUG_LOADERS) d(LOG_TAG, "  ----> applications loader stopped (" + mId + ")");
+                }
             }
             mRunning = false;
         }
@@ -951,40 +953,42 @@ public class LauncherModel {
                 c.close();
             }
 
-            if (!mStopped) {
-                if (DEBUG_LOADERS)  {
-                    d(LOG_TAG, "  --> done loading workspace");
-                    d(LOG_TAG, "  ----> worskpace items=" + desktopItems.size());                
-                    d(LOG_TAG, "  ----> worskpace widgets=" + desktopAppWidgets.size());
-                }
-
-                // Create a copy of the lists in case the workspace loader is restarted
-                // and the list are cleared before the UI can go through them
-                final ArrayList<ItemInfo> uiDesktopItems =
-                        new ArrayList<ItemInfo>(desktopItems);
-                final ArrayList<LauncherAppWidgetInfo> uiDesktopWidgets =
-                        new ArrayList<LauncherAppWidgetInfo>(desktopAppWidgets);
-
+            synchronized(LauncherModel.this) {
                 if (!mStopped) {
-                    d(LOG_TAG, "  ----> items cloned, ready to refresh UI");                
-                    launcher.runOnUiThread(new Runnable() {
-                        public void run() {
-                            if (DEBUG_LOADERS) d(LOG_TAG, "  ----> onDesktopItemsLoaded()");
-                            launcher.onDesktopItemsLoaded(uiDesktopItems, uiDesktopWidgets);
-                        }
-                    });
-                }
-
-                if (mLoadApplications) {
-                    if (DEBUG_LOADERS) {
-                        d(LOG_TAG, "  ----> loading applications from workspace loader");
+                    if (DEBUG_LOADERS)  {
+                        d(LOG_TAG, "  --> done loading workspace");
+                        d(LOG_TAG, "  ----> worskpace items=" + desktopItems.size());
+                        d(LOG_TAG, "  ----> worskpace widgets=" + desktopAppWidgets.size());
                     }
-                    startApplicationsLoader(launcher, mIsLaunching);
-                }
 
-                mDesktopItemsLoaded = true;
-            } else {
-                if (DEBUG_LOADERS) d(LOG_TAG, "  ----> worskpace loader was stopped");
+                    // Create a copy of the lists in case the workspace loader is restarted
+                    // and the list are cleared before the UI can go through them
+                    final ArrayList<ItemInfo> uiDesktopItems =
+                            new ArrayList<ItemInfo>(desktopItems);
+                    final ArrayList<LauncherAppWidgetInfo> uiDesktopWidgets =
+                            new ArrayList<LauncherAppWidgetInfo>(desktopAppWidgets);
+
+                    if (!mStopped) {
+                        d(LOG_TAG, "  ----> items cloned, ready to refresh UI");
+                        launcher.runOnUiThread(new Runnable() {
+                            public void run() {
+                                if (DEBUG_LOADERS) d(LOG_TAG, "  ----> onDesktopItemsLoaded()");
+                                launcher.onDesktopItemsLoaded(uiDesktopItems, uiDesktopWidgets);
+                            }
+                        });
+                    }
+
+                    if (mLoadApplications) {
+                        if (DEBUG_LOADERS) {
+                            d(LOG_TAG, "  ----> loading applications from workspace loader");
+                        }
+                        startApplicationsLoader(launcher, mIsLaunching);
+                    }
+
+                    mDesktopItemsLoaded = true;
+                } else {
+                    if (DEBUG_LOADERS) d(LOG_TAG, "  ----> worskpace loader was stopped");
+                }
             }
             mRunning = false;
         }
